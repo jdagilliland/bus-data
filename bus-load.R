31fdd1239d86a08a1b21ee49c3441349e13ba1df
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2025-10-16 00:09:05 -0400

    Improve data loading and plots

--- bus-load.R
+++ bus-load.R
@@ -2,9 +2,17 @@ library(data.table)
 library(ggplot2)
 library(lubridate)
 library(scales)
+library(purrr)
 
-bus.data <- fread("~/tmp/bus-data.csv", colClasses = c("character","character","character"))
-setnames(bus.data, c("V1", "V2", "V3"), c("date", "departure", "arrival"))
+bus.data.files <- c("data/nashua.csv", "data/north-londonderry.csv")
+bus.data.tables <- map(bus.data.files,
+                       ~ fread(.x,
+                               colClasses = c("character","character","character"),
+                               col.names = c("date", "departure", "arrival")))
+bus.data.nashua <- bus.data.tables[[1]][, station := "nashua"]
+bus.data.nlondonderry <- bus.data.tables[[2]][, station := "north-londonderry"]
+bus.data <- rbind(bus.data.nashua, bus.data.nlondonderry)
+
 bus.data[, departure:=paste(substr(departure, 1, 2), substr(departure,3,4), sep=":")]
 bus.data[, arrival:=paste(substr(arrival, 1, 2), substr(arrival,3,4), sep=":")]
 bus.data[, datetime.departure:=ymd_hm(paste(date, departure, sep="_"))]
@@ -19,6 +27,8 @@ dropDate <- function(x) {
 dropDate2 <- function(x) {
   as.numeric(x - as.Date(x))
 }
+
+# From here on only plots.
 
 plot.durations <- ggplot(bus.data) + aes(x=departure, y=duration) +
     geom_boxplot(outlier.color = NA, fill=NA) + geom_jitter(width=0.3)
@@ -37,6 +47,14 @@ plot.arrivals.1.inbound <- ggplot(bus.data[departure %in%
                                   c("05:40", "06:20", "06:40", "07:20",
                                     "08:00", "09:00", "10:30")]) +
     aes(x=departure, y=as.Date(hm(arrival), origin=lubridate::origin)) +
+    geom_boxplot(outlier.color = NA, fill=NA) + geom_jitter(width=0.3)
+
+plot.arrivals.2.inbound <- ggplot(bus.data[hm(departure) < hm("12:00")]) +
+    aes(x=departure, y=as.Date(hm(arrival), origin=lubridate::origin), color=station) +
+    geom_boxplot(outlier.color = NA, fill=NA) + geom_jitter(width=0.3)
+
+plot.arrivals.1.outbound <- ggplot(bus.data[hm(departure) > hm("12:00")]) +
+    aes(x=departure, y=as.Date(hm(arrival), origin=lubridate::origin), color=station) +
     geom_boxplot(outlier.color = NA, fill=NA) + geom_jitter(width=0.3)
 
 ggsave("plot-durations.png", plot=plot.durations, units="in", height=8.5, width=6)
