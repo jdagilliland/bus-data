2e0d15c0d61b082a7bf92ca02024ece9e0e005b4
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2025-10-16 00:08:32 -0400

    Support RStudio in nix shell

--- shell.nix
+++ shell.nix
@@ -1,15 +1,20 @@
 let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
+  rPacks = with pkgs.rPackages; [
+    data_table
+    tidyverse
+  ];
+  myR = pkgs.rWrapper.override{ packages = rPacks; };
+  myRStudio = pkgs.rstudioWrapper.override { packages = rPacks; };
 in with pkgs; {
   busData = stdenv.mkDerivation {
     name = "bus-data";
     version = "0.1.0.0";
     src = if pkgs.lib.inNixShell then null else nix;
-    buildInputs = with rPackages; [
-      R
-      data_table
-      tidyverse
+    buildInputs = [
+      myR
+      myRStudio
     ];
   };
 }
