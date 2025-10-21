4b2007e9926e3ecec93f4bac122142a4246c9c45
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2025-10-20 23:07:20 -0400

    Pin nixpkgs that allows me to use numpyro

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/11cb3517b3af6af300dd6c055aeda73c9bf52c48.tar.gz";
+    sha256 = "sha256:1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
+  }) {};
+  # pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
   rPacks = with pkgs.rPackages; [
     data_table
@@ -7,6 +11,9 @@ let
   ];
   myR = pkgs.rWrapper.override{ packages = rPacks; };
   myRStudio = pkgs.rstudioWrapper.override { packages = rPacks; };
+  myPy = pkgs.python312.withPackages (pykgs: with pykgs; [
+    numpyro
+  ]);
 in with pkgs; {
   busData = stdenv.mkDerivation {
     name = "bus-data";
@@ -15,6 +22,7 @@ in with pkgs; {
     buildInputs = [
       myR
       myRStudio
+      myPy
     ];
   };
 }
