02ba6cb83b609abaa75d05c93fd830fcdb3f5444
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2025-10-29 18:13:56 -0400

    Add in a bunch of statements from noodling around

--- bus_analysis.py
+++ bus_analysis.py
@@ -1,6 +1,9 @@
+import time
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import arviz as az
 from jax import random, vmap
 import jax.numpy as jnp
 from jax.scipy.special import logsumexp, expit
@@ -8,7 +11,7 @@ import numpyro
 from numpyro import handlers
 from numpyro.diagnostics import hpdi
 import numpyro.distributions as dist
-from numpyro.infer import MCMC, NUTS
+from numpyro.infer import MCMC, NUTS, Predictive
 # from numpyro.contrib.nested_sampling import NestedSampler
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
@@ -21,7 +24,7 @@ def simple_model(bus, duration=None):
     sigma = numpyro.sample("sigma", dist.HalfNormal(40))
     numpyro.sample("obs", dist.Normal(mu[bus], sigma), obs=duration)
 
-def model(station, departure, duration=None):
+def expit_model(station, departure, duration=None):
     region_base = numpyro.sample("region_base", dist.Normal(50, 20))
     hwy_sigma = numpyro.sample("hwy_sigma", dist.Exponential(1.0/20))
     am_rush_hour_start = numpyro.sample("am_rush_hour_start", dist.Normal(17e3, 5e3))
@@ -85,7 +88,7 @@ def fit_rush_hour_model(bus_dset, rng_key_):
     stations_ix = station_le.fit_transform(station_labels)
 
     # Run NUTS.
-    kernel = NUTS(model)
+    kernel = NUTS(expit_model)
     num_samples = 2000
     mcmc = MCMC(kernel, num_warmup=1000, num_samples=num_samples)
     mcmc.run(
@@ -97,21 +100,57 @@ def fit_rush_hour_model(bus_dset, rng_key_):
     # mcmc.print_summary()
     # samples_1 = mcmc.get_samples()
     return mcmc
+    
+def summary_plots(bus_dset, mcmc, buses_le):
+    ax = sns.scatterplot(bus_dset, x="time.departure", y="duration")
+    ax.get_figure().savefig("raw_durations.png")
+    data = az.from_numpyro(mcmc)
+    az.plot_trace(data, figsize=(15, 25))
+    ax.get_figure().savefig("posterior_samples.png")
+    duration_pred_0900_nashua = pred(rng_key, buses_le.transform(np.array(["09:00_nashua"])))["obs"]
+    ax = az.plot_dist(duration_pred_0900_nashua)
+    ax.get_figure().savefig("9am_nashua_duration_prediction.png")
+    ax = az.plot_ecdf(np.asarray(duration_pred_0900_nashua)).copy()
+    ax.get_figure().savefig("9am_nashua_duration_cdf.png")
+    # Chance of a trip on the 0900 Nashua taking less than 80 minutes
+    np.mean(duration_pred_0900_nashua < 80)
+    # Plot hypothetical journeys
+    duration_pred = pred(rng_key, buses_le.transform(buses_le.classes_))["obs"]
+    pred_df = pd.DataFrame(duration_pred.T, index=buses_le.classes_).melt(ignore_index=False, var_name="iteration", value_name="duration")
+    bus_table = bus_dset[['station', 'departure', 'time.departure']].drop_duplicates().sort_values(by="time.departure")
+    bus_table['bus_label'] = bus_table["departure"].str.cat(bus_table["station"], sep="_")
+    bus_table = bus_table.set_index("bus_label")
+    pred_df = pred_df.join(bus_table).reset_index(names="bus_label")
+    ax = sns.violinplot(pred_df, x="bus_label", y="duration", hue="station", inner=None)
+    ax.tick_params(axis="x", rotation=90)
+    ax.get_figure().savefig("violins_hypothetical_journeys.png")
+    bus_summary = bus_dset.groupby("bus_label")["duration"].agg(["mean", "count"]).join(mean_pred.set_axis(["simulated"], axis=1))
+    return bus_summary
 
+def arrive_by(pred_df, arrival):
+    arr = time.strptime(arrival, "%H:%M")
+    time_arrival = arr.tm_hour * 3600 + arr.tm_min * 60 + arr.tm_sec
+    pred_df["time.arrival"] = pred_df["time.departure"] + 60 * pred_df["duration"]
+    pred_df["ontime"] = pred_df["time.arrival"] < time_arrival
+    return pred_df.groupby("bus_label")["ontime"].mean()
+    
 def main():
     # Data setup
     bus_dset = pd.read_csv("bus-data.csv")
+    bus_dset["bus_label"] = bus_dset["departure"].str.cat(bus_dset["station"], sep="_")
      # Start from this source of randomness. We will split keys for subsequent operations.
     rng_key = random.PRNGKey(0)
-    rng_key, rng_key_ = random.split(rng_key)
-
-    # mcmc_simple = fit_simple_model(bus_dset, rng_key_)
 
     rng_key, rng_key_ = random.split(rng_key)
+    mcmc_simple = fit_simple_model(bus_dset, rng_key_)
 
-    mcmc_rush_hour = fit_rush_hour_model(bus_dset, rng_key_)
+    # rng_key, rng_key_ = random.split(rng_key)
+    # mcmc_rush_hour = fit_rush_hour_model(bus_dset, rng_key_)
 
-    return mcmc_simple, mcmc_rush_hour
+    return (
+            mcmc_simple,
+            # mcmc_rush_hour,
+            )
 
 if __name__ == "__main__":
-    mcmc = main()
+    (mcmc,) = main()
